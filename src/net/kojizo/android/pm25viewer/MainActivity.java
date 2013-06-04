580b52848a30b4bd5b7fa359e9a026f0847e1fc2
nakaji <nakaji126@gmail.com>
2013-06-04 21:43:44 +0900

    3時間進む、戻るボタンを追加

--- src/net/kojizo/android/pm25viewer/MainActivity.java
+++ src/net/kojizo/android/pm25viewer/MainActivity.java
@@ -10,6 +10,9 @@ import android.os.Bundle;
 import android.os.StrictMode;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends Activity {
@@ -17,6 +20,8 @@ public class MainActivity extends Activity {
     String fileNameFormat = "japan_detail_%04d-%02d-%02d-%02d-00-00_large.jpg";
     String BaseUrl = "http://guide.tenki.jp/static_images/particulate_matter/japan_detail/";
     Calendar _cal;
+
+    private final MainActivity mainActivity = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +31,7 @@ public class MainActivity extends Activity {
         //StrictModeを設定 penaltyDeathを取り除く
         StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().detectAll().penaltyLog().build());
 
-        _cal = Calendar.getInstance();
+        calendarInit();
 
         Drawable drawable = getDrawableByCalendar();
 
@@ -34,13 +39,55 @@ public class MainActivity extends Activity {
 
         imageView.setImageDrawable(drawable);
 
+        Button prev = (Button) findViewById(R.id.buttonPrev);
+        prev.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                _cal.add(Calendar.HOUR, -3);
+
+                Drawable drawable = getDrawableByCalendar();
+
+                ImageView imageView = (ImageView) mainActivity.findViewById(R.id.imageView);
+
+                imageView.setImageDrawable(drawable);
+            }
+        });
+
+        Button next = (Button) findViewById(R.id.buttonNext);
+        next.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                _cal.add(Calendar.HOUR_OF_DAY, 3);
+
+                Drawable drawable = getDrawableByCalendar();
+
+                ImageView imageView = (ImageView) mainActivity.findViewById(R.id.imageView);
+
+                imageView.setImageDrawable(drawable);
+            }
+        });
+    }
+
+    private void calendarInit() {
+        Calendar now = Calendar.getInstance();
+        int year = now.get(Calendar.YEAR);
+        int month = now.get(Calendar.MONTH) + 1;
+        int date = now.get(Calendar.DATE);
+        int hour = (int) (Math.floor(now.get(Calendar.HOUR_OF_DAY) / 3) * 3 + 3);
+
+        _cal = Calendar.getInstance();
+        _cal.set(year, month, date, hour, 0);
     }
 
     private Drawable getDrawableByCalendar() {
         int year = _cal.get(Calendar.YEAR);
-        int month = _cal.get(Calendar.MONTH) + 1;
+        int month = _cal.get(Calendar.MONTH);
         int date = _cal.get(Calendar.DATE);
-        int hour = (int) (Math.floor(_cal.get(Calendar.HOUR) / 3)*3+3);
+        int hour = _cal.get(Calendar.HOUR_OF_DAY);
 
         String fileName = String.format(fileNameFormat, year, month, date, hour);
 
