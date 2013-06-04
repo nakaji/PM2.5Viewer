4846aac364ac30e66ad3e418376e48b24bf922a3
nakaji <nakaji126@gmail.com>
2013-06-04 21:56:35 +0900

    ソースを整理

--- src/net/kojizo/android/pm25viewer/MainActivity.java
+++ src/net/kojizo/android/pm25viewer/MainActivity.java
@@ -17,9 +17,9 @@ import android.widget.ImageView;
 
 public class MainActivity extends Activity {
 
-    String fileNameFormat = "japan_detail_%04d-%02d-%02d-%02d-00-00_large.jpg";
-    String BaseUrl = "http://guide.tenki.jp/static_images/particulate_matter/japan_detail/";
-    Calendar _cal;
+    private final String fileNameFormat = "japan_detail_%04d-%02d-%02d-%02d-00-00_large.jpg";
+    private final String BaseUrl = "http://guide.tenki.jp/static_images/particulate_matter/japan_detail/";
+    private Calendar _cal;
 
     private final MainActivity mainActivity = this;
 
@@ -33,11 +33,7 @@ public class MainActivity extends Activity {
 
         calendarInit();
 
-        Drawable drawable = getDrawableByCalendar();
-
-        ImageView imageView = (ImageView) this.findViewById(R.id.imageView);
-
-        imageView.setImageDrawable(drawable);
+        drawMap();
 
         Button prev = (Button) findViewById(R.id.buttonPrev);
         prev.setOnClickListener(new OnClickListener() {
@@ -47,11 +43,7 @@ public class MainActivity extends Activity {
 
                 _cal.add(Calendar.HOUR, -3);
 
-                Drawable drawable = getDrawableByCalendar();
-
-                ImageView imageView = (ImageView) mainActivity.findViewById(R.id.imageView);
-
-                imageView.setImageDrawable(drawable);
+                drawMap();
             }
         });
 
@@ -63,15 +55,26 @@ public class MainActivity extends Activity {
 
                 _cal.add(Calendar.HOUR_OF_DAY, 3);
 
-                Drawable drawable = getDrawableByCalendar();
-
-                ImageView imageView = (ImageView) mainActivity.findViewById(R.id.imageView);
-
-                imageView.setImageDrawable(drawable);
+                drawMap();
             }
         });
     }
 
+    /*
+     * 画面に地図を描画する
+     */
+    private void drawMap() {
+        Drawable drawable = getDrawableByCalendar();
+
+        ImageView imageView = (ImageView) this.findViewById(R.id.imageView);
+
+        imageView.setImageDrawable(drawable);
+    }
+
+    /*
+     * 内蔵カレンダーを直近の3時間単位で初期化
+     * 例）22:00に実行された場合は0:00
+     */
     private void calendarInit() {
         Calendar now = Calendar.getInstance();
         int year = now.get(Calendar.YEAR);
@@ -83,6 +86,9 @@ public class MainActivity extends Activity {
         _cal.set(year, month, date, hour, 0);
     }
 
+    /*
+     * 内蔵カレンダーの時刻を元に、PM2.5分布予測の画像を取得する
+     */
     private Drawable getDrawableByCalendar() {
         int year = _cal.get(Calendar.YEAR);
         int month = _cal.get(Calendar.MONTH);
@@ -94,15 +100,16 @@ public class MainActivity extends Activity {
         Drawable drawable = getDrawableFromInternet(BaseUrl + fileName);
 
         return drawable;
-
     }
 
+    /*
+     * Webから画像を取得してDrawableを返す
+     */
     private Drawable getDrawableFromInternet(String urlString) {
         URL url;
         try {
             url = new URL(urlString);
             InputStream istream = url.openStream();
-            //Drawableが必要な場合
             Drawable drowable = Drawable.createFromStream(istream, "webimg");
             return drowable;
         } catch (Exception e) {
